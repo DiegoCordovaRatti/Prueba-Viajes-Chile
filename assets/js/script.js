dcf68c53b9211b1d6158585caebdba0f521f7540
Diego <d.cordovaratti@gmail.com>
2021-06-17 23:15:37 -0300

    fixed scroll & carousel - added modals - modified cards layout - hide section 'quienes somos'

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,20 @@
 /* Enable tooltips */
 $(function () {
     $('[data-toggle="tooltip"]').tooltip({
-        delay: { "show": 300, "hide": 100 } /* adds delay to the tooltip */
+        delay: {
+            "show": 300,
+            "hide": 100
+        } /* adds delay to the tooltip */
     });
-  });
+});
 
 /* smooth scroll */
-$('a').click(function(){
-    $('html, body').animate({
-        scrollTop: $( $(this).attr('href') ).offset().top
-    }, 500);
-    return false;
-});	
+$(function () {
+    let sc = document.getElementsByClassName("smooth")
+    $(sc).click(function () {
+        $('html, body').animate({
+            scrollTop: $($(this).attr('href')).offset().top
+        }, 500);
+        return false;
+    });
+});
